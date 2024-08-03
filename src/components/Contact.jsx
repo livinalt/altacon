217b876834eae94a1b2c9f6042077742e3d951d1
Jeremiah Samuel <livinalt@gmail.com>
2024-08-03 14:53:56 +0100

    fixed contact layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Contact = () => {
   return (
-    <div className="flex flex-col lg:flex-row justify-between bg-[#093262] text-white p-8 sm:p-12 lg:p-16 h-auto lg:h-[60vh]">
-      <div className="mb-8 lg:mb-0 lg:w-1/2">
+    <div className="flex flex-col lg:flex-row justify-between items-center lg:items-center bg-[#093262] text-white p-8 sm:p-12 lg:p-16 h-[60vh]">
+      <div className="mb-8 lg:mb-0 lg:w-1/2 text-center lg:text-left">
         <h3 className="text-lg sm:text-xl mb-4">Contact</h3>
         <p className="mb-6">Have ideas you want to collaborate on?</p>
         <button className="px-6 sm:px-10 py-2 border rounded-3xl hover:bg-blue-700 transition-all">
@@ -22,9 +22,9 @@ const Contact = () => {
         </button>
       </div>
 
-      <div className="lg:w-1/2">
+      <div className="lg:w-1/2 text-center lg:text-left">
         <h3 className="text-lg sm:text-xl mb-4">Links</h3>
-        <div className="flex flex-wrap gap-4 text-sm">
+        <div className="flex flex-wrap justify-center lg:justify-start gap-4 text-sm">
           <a href="" className="hover:text-blue-400 transition-colors">
             About
           </a>
